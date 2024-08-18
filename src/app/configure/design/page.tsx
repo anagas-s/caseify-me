b2186c8dad0201a986b97205e24f93a166f50a92
Anagas <anagassatheeshkumar@gmail.com>
2024-08-18 11:18:51 +0530

    Create configuration page

--- src/app/configure/design/page.tsx
+++ src/app/configure/design/page.tsx
@@ -1,7 +1,35 @@
+import { notFound } from "next/navigation";
 import React from "react";
+import { db } from "@/db";
+import DesignConfigurator from "./DesignConfigurator";
 
-const Page = async () => {
-  return <div>Page</div>;
+interface PageProps {
+  searchParams: {
+    [key: string]: string | string[] | undefined;
+  };
+}
+
+const Page = async ({ searchParams }: PageProps) => {
+  const { id } = searchParams;
+
+  if (!id || typeof id !== "string") {
+    return notFound();
+  }
+  const configuration = await db.configuration.findUnique({
+    where: { id },
+  });
+  if (!configuration) {
+    return notFound();
+  }
+  const { imageUrl, width, height } = configuration;
+
+  return (
+    <DesignConfigurator
+      configId={configuration.id}
+      imageDimensions={{ width, height }}
+      imageUrl={imageUrl}
+    />
+  );
 };
 
 export default Page;
